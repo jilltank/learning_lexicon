39319a9278ea491c589acdc2e2e4c3f674937d90
Jill Tankersley <jillian.emilie@gmail.com>
2016-02-14 10:52:26 -0500

    minor fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,22 @@ path = "/users/jilli/desktop/lexicon_db"
 Dir.foreach(path) do |file|
 	if file.include? ('.csv')
 		CSV.foreach ("#{path}/#{file}") do |row| 
-			Lexicon.create(category:file, code:row[0], description:row[1])
+			case file
+			when "html.csv"
+				new_cat = "HTML"
+			when "css.csv"
+				new_cat = "CSS"
+			when "git_console.csv"
+				new_cat = "Git & Console Commands"
+			when "jquery.csv"
+				new_cat = "jQuery"
+			when "javascript.csv"
+				new_cat = "JavaScript"
+			else
+				foo = file.split(".")
+				new_cat = foo[0].capitalize
+			end
+			Lexicon.find_or_create_by(category:new_cat, code:row[0], description:row[1])
 		end
 	end
 end
