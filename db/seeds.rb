c1a6046abbefeffb6e3998f8e71445287008ce4e
Jill Tankersley <jillian.emilie@gmail.com>
2016-02-15 12:32:57 -0500

    working on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 require 'csv'
-path = "/users/jilli/desktop/lexicon_db"
+path = "#{Rails.root.to_s}/users/jilli/desktop/lexicon_db"
 
 Dir.foreach(path) do |file|
 	if file.include? ('.csv')
