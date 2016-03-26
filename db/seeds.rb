c2e9a2087452b8337e16b65084750296586436e6
Jillian Tankersley <jillian.emilie@gmail.com>
2016-03-26 13:47:47 -0400

    more activeadmin fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require 'csv'
-path = "#{Rails.root.to_s}/lib/assets/lexicon_db"
+# require 'csv'
+# path = "#{Rails.root.to_s}/lib/assets/lexicon_db"
 
-Dir.foreach(path) do |file|
-	if file.include? ('.csv')
-		CSV.foreach ("#{path}/#{file}") do |row| 
-			case file
-			when "html.csv"
-				new_cat = "HTML"
-			when "css.csv"
-				new_cat = "CSS"
-			when "git_console.csv"
-				new_cat = "Git & Console Commands"
-			when "jquery.csv"
-				new_cat = "jQuery"
-			when "javascript.csv"
-				new_cat = "JavaScript"
-			else
-				foo = file.split(".")
-				new_cat = foo[0].capitalize
-			end
-			Lexicon.find_or_create_by(category:new_cat, code:row[0], description:row[1])
-		end
-	end
-end
+# Dir.foreach(path) do |file|
+# 	if file.include? ('.csv')
+# 		CSV.foreach ("#{path}/#{file}") do |row| 
+# 			case file
+# 			when "html.csv"
+# 				new_cat = "HTML"
+# 			when "css.csv"
+# 				new_cat = "CSS"
+# 			when "git_console.csv"
+# 				new_cat = "Git & Console Commands"
+# 			when "jquery.csv"
+# 				new_cat = "jQuery"
+# 			when "javascript.csv"
+# 				new_cat = "JavaScript"
+# 			else
+# 				foo = file.split(".")
+# 				new_cat = foo[0].capitalize
+# 			end
+# 			Lexicon.find_or_create_by(category:new_cat, code:row[0], description:row[1])
+# 		end
+# 	end
+# end
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
